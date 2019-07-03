35120ec13c961182134f3d755c0bec5d6b85a9eb
Peter Holder <peter.s.holder@boeing.com>
2019-07-03 18:02:39 -0500

    Upload HW3 Final

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,111 @@
+# Using a single open
+# --------Peter Holder HW3 Python (PyBank)-------------------- 
+# This program analyzes Financial Data for my company
+import csv
+csvpath = "../Resources/budget_data.csv"
+#File import and opening
+with open (csvpath, newline="") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter =',')
+#Skip the header move to the first row of data
+    header_row = next(csv_reader)
+    total_profit_loss = 0
+    max_incr_profit = 0
+    max_decr_profit = 0
 
+# Calculate the total number of months included in the dataset
+    budget_data_rows = list(csv_reader)
+    total_months = len(budget_data_rows)
+    
+# Calculate the total amount of "Profit/Losses" over the entire period
+    #total_profit_loss = sum(int(row[1]) for row in csv.reader(csv_file))
+    first_iteration = True
+    delta_list = []
+    for row in budget_data_rows:
+        curr_pl = float(row[1])
+        total_profit_loss = total_profit_loss + curr_pl
+        if first_iteration == True:
+            prev_pl = curr_pl
+            first_iteration = False         
+        else:
+            delta_pl = curr_pl - prev_pl
+            delta_list.append(delta_pl)
+            if delta_pl > max_incr_profit:
+                max_incr_profit = delta_pl
+                max_incr_month = row[0]
+            elif delta_pl < max_decr_profit:
+                max_decr_profit = delta_pl
+                max_decr_month = row[0]
+            prev_pl = curr_pl
+
+avg_profit_loss = sum(delta_list) / len(delta_list)
+
+#
+print("              Financial Analysis                  ")    
+print("--------------------------------------------------")
+print(f"The total number of months are:    {total_months}")
+print(f"The total_profit_loss is: ${total_profit_loss:.2f}")
+print(f"The average profit/loss is: ${avg_profit_loss:.2f}") 
+print(f"The maximum profit increase is:{max_incr_month} ${max_incr_profit:.2f}")
+print(f"The maximum profit decrease is:{max_decr_month} $({max_decr_profit:.2f})")
+
+f = open('Financial_Analysis.txt', 'w')
+f.write("%s\n" % "Financial Analysis")
+f.write("%s\n" % "----------------------------")
+Total_Months = "Total Months: " + str(total_months) + "\n"
+f.write(Total_Months)
+tot_pl = "Total: $" + str(int(total_profit_loss)) + "\n"
+f.write(tot_pl)
+delta_chg = "Average  Change: $" + str(round(avg_profit_loss, 2)) + "\n"
+f.write(delta_chg)
+max_info = "Greatest Increase in Profits: " + str(max_incr_month) + "  ($" + str(max_incr_profit) + ")\n"
+f.write(max_info)
+min_info = "Greatest Decrease in Profits: " + str(max_decr_month) + "  ($" + str(max_decr_profit) + ")\n"
+f.write(min_info)
+
+f.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Calculate the average of the changes in "Profit/Losses"over the entire period    
+#first_iteration = True
+
+#if first_iteration == True:
+#    curr_profit_loss = prev_profit_loss
+#    budget_data_rows.append
+#else:
+#    curr_profit_loss = curr_profit_loss - prev_profit_loss
+#    curr_profit_loss = prev_profit_loss    
+
+# Financial Analysis Report print out
+
+    
+     
+     
+
+
+    
+       
+
+        
+   
+
+# Calculate the greatest increase in profits (date and amount) over the entire period
+
+
+
+
+
+# Calculate the greatest decrease in losses (date and amount) over the entire period
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,82 @@
+# Using a single open
+# --------Peter Holder HW3 Python (PyPoll)-------------------- 
+# This program analyzes Election Results Data 
+import csv
+import operator  
+csvpath = "../Resources/election_data.csv"
+#File import and opening
+with open (csvpath, newline="") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter =',')
+    #Skip the header move to the first row of data
+    header_row = next(csv_reader)
 
+# Calculate the total number of votes cast
+    election_data = list(csv_reader)
+    total_votes = len(election_data)
+
+# Determine A complete list of candidates who received votes
+    first_iteration = True
+#Create a sorted list of the candidates on candidate name    
+    candidate_list = sorted(election_data, key=operator.itemgetter(2))
+
+#Intialize a new list to store candidate voting results
+election_results = []
+
+hold_candidate = ""
+vote_count = 0
+
+#Populate election results list from candidates list who received votes    
+for row in candidate_list:
+    if first_iteration == True:
+        hold_candidate = row[2]
+        vote_count += 1
+        first_iteration = False         
+    elif hold_candidate == row[2]:
+        vote_count += 1
+    else:
+        cand_vote_pct = float(round((vote_count/total_votes)* 100, 3))
+        election_results.append([hold_candidate, cand_vote_pct, vote_count])
+        hold_candidate = row[2]
+        vote_count = 1
+
+# Capture the last candidates voting results and write to election results list
+cand_vote_pct = float(round((vote_count/total_votes)* 100, 3))
+election_results.append([hold_candidate, cand_vote_pct, vote_count])
+
+election_results = sorted(election_results, key=operator.itemgetter(2), reverse=True)
+
+# Display Election Results to the screen
+candidate_votes = 0
+winner = ""
+print("Election Results") 
+print("-------------------------")
+print(f"Total Votes: {str(total_votes)} ")
+print("-------------------------")
+#List out the voting results from the Election Results list
+first_iteration = True
+Winner = ""
+for row in election_results:
+    print(f"{row[0]}: {str(float(round(row[1],3)))}% {str(int(row[2]))} ")
+    if first_iteration:
+        Winner = row[0]
+        first_iteration = False
+print("-------------------------")
+print(f"Winner: {str(Winner)} ")
+print("-------------------------")
+
+#Open output file to write election results
+f = open('Election_Results.txt', 'w')
+f.write("%s\n" % "Election Results")
+f.write("%s\n" % "-------------------------")
+Total_Votes = "Total Votes: " + str(total_votes) + "\n"
+f.write(Total_Votes)
+f.write("%s\n" % "-------------------------")
+for i, row in enumerate(election_results):
+    candidate_line = str(row[0]) + ": " + str(float(round(row[1], 3))) + "% (" + str(row[2]) + ") \n"
+    f.write(candidate_line)
+f.write("%s\n" % "-------------------------")
+winner_line = "Winner: " + Winner + "\n"
+f.write(winner_line)
+f.write("%s\n" % "-------------------------")
+
+f.close()
